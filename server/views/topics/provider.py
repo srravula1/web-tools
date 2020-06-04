0c9f2488a25de4b704cf87510b1872c7cf93a518
Rahul B <rahulbot@gmail.com>
2020-06-04 11:37:30 -0400

    Fix topic story list download  #1866

--- server/views/topics/provider.py
+++ server/views/topics/provider.py
@@ -89,7 +89,9 @@ def topic_provider_stories_csv(topics_id):
     optional_args = _parse_stories_optional_arguments()
     user_mc = user_mediacloud_client()
     topic = user_mc.topic(topics_id)
-    del optional_args['link_id']  # we do this do make sure this helper can page through the results
+    # we do this do make sure this helper can page through the results (because this is a download request)
+    if 'link_id' in optional_args:
+        del optional_args['link_id']
     return stream_story_list_csv(user_mediacloud_key(), 'stories', topic, **optional_args)
 
 
